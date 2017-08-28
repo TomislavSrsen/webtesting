747336ea0e613d47acaa43c62ce369d21d5956e9
TomislavSrsen <31403280+TomislavSrsen@users.noreply.github.com>
2017-08-28 10:46:27 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pocetna stranica</title>
+</head>
+<body>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Klikni za prikaz datuma i vremena.</button>
+<p id='demo'></p>
+<p>Linkovi na fajlove:</p>
+<a href='dcb.pdf'>Link na jedan od mojih osobnih prezentacija u vezi Direct Carrier Billing-a</a>
+<a href=tomislav_cv.pdf> Moj CV</a>
+
+<script>
+window.alert('Dobrodosli - Testing browser side scriptinga');
+</script>
+</body>
+</html>
