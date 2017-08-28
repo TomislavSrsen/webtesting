62a4300d35dc0d06e1f9b53e79afe9b39030f2a7
Tomislav Srsen <31403280+TomislavSrsen@users.noreply.github.com>
2017-08-28 12:09:42 +0200

    Update index.php
    Update

--- index.php
+++ index.php
@@ -2,15 +2,38 @@
 <html>
 <head>
 <title>Pocetna stranica</title>
+<style>
+.linkovi {
+float : left;
+margin : 10px;
+padding : 10px;
+max-width : 300px;
+height : 500px;
+border: 5px solid red;
+}
+a {
+text-decoration: none;
+}
+
+</style>
 </head>
 <body>
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
 Klikni za prikaz datuma i vremena.</button>
 <p id='demo'></p>
+<div class='linkovi'>
 <p>Linkovi na fajlove:</p>
-<a href='dcb.pdf'>Link na jedan od mojih osobnih prezentacija u vezi Direct Carrier Billing-a</a>
-<a href=tomislav_cv.pdf> Moj CV</a>
+<a href='dcb.pdf'>Link na jedan od mojih osobnih prezentacija u vezi Direct Carrier Billing-a</a> 
+<p>
+<a href='tomislav_cv.pdf'> Moj CV</a></p>
+<p><a href='tmpreporuka.pdf'>Preporuka - Tisak Media</a></p>
+</div>
+
+<p>
+<img src='opglogo.jpg'
+style='width:300px; height:200px;'>
+</p>
 
 <script>
 window.alert('Dobrodosli - Testing browser side scriptinga');
